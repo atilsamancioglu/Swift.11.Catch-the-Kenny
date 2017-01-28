ee6be4b598f373fca56279609cca92649fa6f0a3
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2017-01-28 13:23:02 +0300

    Initial Commit

---
+++ Catch the Kenny/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Catch the Kenny
+//
+//  Created by Atıl Samancıoğlu on 28/01/2017.
+//  Copyright © 2017 Atıl Samancıoğlu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
